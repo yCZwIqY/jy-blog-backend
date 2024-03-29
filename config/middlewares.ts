cbe689f96440a953192d176dc94723383643a339
jiyoon0605 <ljy10499@gmail.com>
2024-03-29 23:22:57 +0900

    tiny editor 적용

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,37 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "script-src": ["'self'", "*.tinymce.com", "*.tiny.cloud", "https:"],
+          "connect-src": ["'self'", "*.tinymce.com", "*.tiny.cloud", "blob:", "*.strapi.io"],
+          "img-src": [
+            "'self'",
+            "*.tinymce.com",
+            "*.tiny.cloud",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "strapi.io",
+            "s3.amazonaws.com",
+            "cdn.jsdelivr.net",
+          ],
+          "style-src": [
+            "'self'",
+            "'unsafe-inline'",
+            "*.tinymce.com",
+            "*.tiny.cloud",
+          ],
+          "font-src": ["'self'", "*.tinymce.com", "*.tiny.cloud"],
+        },
+        upgradeInsecureRequests: null,
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- config/plugins.ts
+++ config/plugins.ts
@@ -40,4 +40,47 @@ export default () => ({
       }
     }
   },
+  tinymce: {
+    enabled: true,
+    config: {
+      editor: {
+        outputFormat: "html",
+        editorConfig: {
+          language: "sk",
+          height: 500,
+          menubar: false,
+          extended_valid_elements: "span, img, small",
+          forced_root_block: "",
+          convert_urls: false,
+          entity_encoding: "raw",
+          plugins: "anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown",
+          toolbar: "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+          style_formats: [
+            {
+              title: "Headings",
+              items: [
+                { title: "h1", block: "h1" },
+                { title: "h2", block: "h2" },
+                { title: "h3", block: "h3" },
+                { title: "h4", block: "h4" },
+                { title: "h5", block: "h5" },
+                { title: "h6", block: "h6" },
+              ],
+            },
+
+            {
+              title: "Text",
+              items: [
+                { title: "Paragraph", block: "p" },
+                {
+                  title: "Paragraph with small letters",
+                  block: "small",
+                },
+              ],
+            },
+          ],
+        },
+      },
+    },
+  },
 })
