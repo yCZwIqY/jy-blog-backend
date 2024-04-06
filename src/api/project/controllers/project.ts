89ef869dfd62c387216ed942fdec4fd8399e737b
jiyoon0605 <ljy10499@gmail.com>
2024-04-07 00:51:37 +0900

    data 변경

--- src/api/project/controllers/project.ts
+++ src/api/project/controllers/project.ts
@@ -5,3 +5,20 @@
 import { factories } from '@strapi/strapi'
 
 export default factories.createCoreController('api::project.project');
+
+module.exports = factories.createCoreController('api::project.project', ({strapi}) => ({
+  async findOne(ctx) {
+    const {id: slug} = ctx.params;
+    const {query} = ctx;
+    if (!query.filters) { // @ts-ignore
+      query.filters = {}
+    }
+    // @ts-ignore
+    query.filters.slug = {'$eq': slug}
+    const entity = await strapi.service('api::project.project').find(query);
+    // @ts-ignore
+    const {results} = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(results[0]);
+  }
+}))
